4cccf7c96e6810069888b3444a3115b77c845b5d
paul2011 <33507393+paul2011@users.noreply.github.com>
2017-11-08 21:55:48 -0800

    To Bypass New Plugin Manager

--- core-minicomponents/j16000plugin_manager_check.class.php
+++ core-minicomponents/j16000plugin_manager_check.class.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Core file.
- *
- * @author Vince Wooll <sales@jomres.net>
- *
- * @version Jomres 9.9.15
- *
- * @copyright	2005-2017 Vince Wooll
- * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
- **/
 
 // ################################################################
 defined('_JOMRES_INITCHECK') or die('');
@@ -25,34 +15,10 @@ class j16000plugin_manager_check
 
             return;
         }
-
-		$this->retVals = '';
-        if ( !file_exists(JOMRES_COREPLUGINS_ABSPATH.'plugin_manager'.JRDS.'plugin_info.php') ) {
-			if (isset($componentArgs[ 'output_now' ])) {
-				$output_now = $componentArgs[ 'output_now' ];
-			} else {
-				$output_now = true;
-			}
-			$output = array();
-			$pageoutput = array();
-			$output['MESSAGE'] = 'The Plugin Manager is not installed, you must install it through the Plugin Manager page to use Jomres Core plugins or install third party plugins.';
-
-			$pageoutput[ ] = $output;
-			$tmpl = new patTemplate();
-			$tmpl->setRoot(JOMRES_TEMPLATEPATH_ADMINISTRATOR);
-			$tmpl->addRows('pageoutput', $pageoutput);
-			$tmpl->readTemplatesFromInput('plugin_manager_warning.html');
-
-			if ($output_now) {
-				$tmpl->displayParsedTemplate();
-			} else {
-				$this->retVals = $tmpl->getParsedTemplate();
-			}
-		
+$this->retVals = '';
+				
 		}
 
-
-    }
 
     // This must be included in every Event/Mini-component
     public function getRetVals()
--- core-minicomponents/j16000prices.class.php
+++ core-minicomponents/j16000prices.class.php
@@ -4,34 +4,25 @@
 defined('_JOMRES_INITCHECK') or die('');
 // ################################################################
 
-class j16000show_license_message
+class j16000prices
 {
     public function __construct($componentArgs)
     {
-        jr_import('minicomponent_registry');
+        // Must be in all minicomponents. Minicomponents with templates that can contain editable text should run $this->template_touch() else just return
         $MiniComponents = jomres_singleton_abstract::getInstance('mcHandler');
-        $this->retVals = '';
         if ($MiniComponents->template_touch) {
             $this->template_touchable = false;
 
             return;
         }
-
-        if (isset($componentArgs[ 'output_now' ])) {
-            $output_now = $componentArgs[ 'output_now' ];
-        } else {
-            $output_now = true;
-        }
-        if (defined('LICENSE_EXPIRED_MESSAGE')) {
-            $this->retVals = LICENSE_EXPIRED_MESSAGE;
-        } else {
-            $this->retVals = '';
-        }
+		
+		return;
     }
+
 
     // This must be included in every Event/Mini-component
     public function getRetVals()
     {
-        return $this->retVals;
+        return null;
     }
 }
